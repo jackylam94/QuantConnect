4470a73ae4fe00b0259b4fb53a7030b4e598abe4
Stefano Raggi <stefano.raggi67@gmail.com>
2021-05-17 17:37:45 +0200

    Trigger build

--- Brokerages/Brokerage.cs
+++ Brokerages/Brokerage.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
  * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
  *
@@ -371,6 +371,5 @@ namespace QuantConnect.Brokerages
         }
 
         #endregion
-
     }
 }
